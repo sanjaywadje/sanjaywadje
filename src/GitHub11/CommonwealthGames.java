61d4d64c558b105e3144b5e1325adc5caf1eb573
Windows-10 <Windows-10@DESKTOP-AUHNTHL>
2022-08-07 17:41:19 +0530

    Pushing gitHub1 project.

---
+++ src/GitHub11/CommonwealthGames.java
@@ -0,0 +1,9 @@
+package GitHub11;
+
+public class CommonwealthGames {
+
+	public static void main(String[] args) {
+       System.out.println("Common Welath Games conducting at Burminham.");
+	}
+
+}
